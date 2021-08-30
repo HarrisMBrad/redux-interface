2557e0f9b448056e392fcdffe535beb22fe8f323
HarrisMBrad <bmichaelh13@gmail.com>
2021-08-30 13:58:12 -0400

    added more pass/fails and debug props

--- src/components/courses/CourseForm.Enzym.test.js
+++ src/components/courses/CourseForm.Enzym.test.js
@@ -18,7 +18,7 @@ function renderCourseForm(args) {
 
 it("renders form and header", () => {
     const wrapper = renderCourseForm();
-    // console.log(wrapper.debug());
+    console.log(wrapper.debug());
     expect(wrapper.find("form").length).toBe(1);
     expect(wrapper.find("h2").text()).toEqual("Add Course");
 });

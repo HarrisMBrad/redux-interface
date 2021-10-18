7f1ca2558c01fead5616720043cb7dd2597e160e
HarrisMBrad <bmichaelh13@gmail.com>
2021-10-18 09:12:05 -0400

    updated loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -34,10 +34,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        test: /\.csss|css$/,
+        // eslint-disable-next-line no-undef
+        exclude: ["/node_modules/"],
         use: ["babel-loader", "eslint-loader"]
       },
+
+
       {
         test: /(\.css)$/,
         use: ["style-loader", "css-loader"]
